c9b436a2ee26b4302ec9fd1a2e6457c51b3f60d7
Oliver Strickson <ostrickson@turing.ac.uk>
2021-11-14 18:44:18 +0000

    define/backprop

--- rackpropagator-lib/rackpropagator/derivative.rkt
+++ rackpropagator-lib/rackpropagator/derivative.rkt
@@ -17,7 +17,6 @@
 
 (provide D+)
 
-
 (define (unknown-backprop op)
   (raise-arguments-error 'prim-definition
                          "Backpropagator unknown"
@@ -77,3 +76,46 @@
                        ;; drop terms from closed-over variables
                        (cdr (apply (backprop primal+backprop) Aw))
                        xs)))))))))]))
+
+;; TODO better name!
+;;
+;; This is like D+, but does not:
+;;  - syntax-parameterize with current-non-prim, box-adjoints
+;;  - drop closure terms
+;;  - apply coerce-zero to the result
+;;
+;; It is more 'composable' and is intended to write some
+;; differentiation primitives
+(define-syntax (D* stx)
+  (syntax-parse stx
+    #:literal-sets (kernel-literals)
+    [(_ e)
+     #:with e-anf
+     (set!->set-box! (anf-normalize (local-expand #'e 'expression '())))
+
+     #:with (let-values (((x-result-arg) e*)) x-result-final) #'e-anf
+     #:fail-unless (free-identifier=? #'x-result-arg #'x-result-final)
+     "Result of anf-normalize had an unexpected form"
+
+     #:with (De* (prim:id prim-intro:id) ...) (reverse-transform #'e*)
+
+     #:with ((distinct-prim distinct-prim-intro) ...)
+     (remove-duplicates (syntax-e #'((prim prim-intro) ...))
+                        free-identifier=? #:key stx-car)
+
+     #:with (prim-def ...) #'((prim-definition distinct-prim) ...)
+
+     #'(let ([distinct-prim-intro prim-def] ...) De*)]))
+
+
+(define-syntax (define/backprop stx)
+  (syntax-parse stx
+    ;; TODO formals
+    [(_ (f xs ...) body ...)
+     #:with (xs* ...) (generate-temporaries #'(xs ...))
+     #:with D*f (local-expand #'(D* (λ (xs* ...) (define (f xs ...) body ...) (f xs* ...)))
+                              'expression
+                              #f)
+     #'(begin
+         (define (f xs ...) body ...)
+         (local-register-primitive! f D*f))]))
