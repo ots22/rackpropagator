8d690404979c29c705cf06e58e0b0d74f93efb1a
Oliver Strickson <ostrickson@turing.ac.uk>
2021-12-15 16:22:05 +0000

    Clean up prim-definition

--- rackpropagator-lib/rackpropagator/prim-definition.rkt
+++ rackpropagator-lib/rackpropagator/prim-definition.rkt
@@ -13,53 +13,12 @@
 
 (provide local-register-primitive!
          register-primitive!
-         prim-definition
          backprop-out
          require/primal+backprop
          require/backprop
          (for-syntax get-prim-definition
                      set-prim-definition!))
 
-(define-for-syntax prim-table (make-free-id-table))
-
-(define-syntax (local-register-primitive! stx)
-  (syntax-parse stx
-    [(_ prim-id prim-augmented-def)
-     (dict-set! prim-table #'prim-id #'prim-augmented-def)
-     #'(void)]))
-
-(define-for-syntax (set-prim-definition! prim-id prim-augmented-def)
-  (dict-set! prim-table prim-id prim-augmented-def))
-
-(define-syntax (register-primitive! stx)
-  (syntax-parse stx
-    [(_ prim-id)
-     #:with prim-augmented-def (dict-ref prim-table #'prim-id)
-     #'(begin-for-syntax
-         (dict-set! prim-table #'prim-id #'prim-augmented-def))]))
-
-(define-for-syntax (get-prim-definition id)
-  (dict-ref prim-table id #f))
-
-(define-syntax (prim-definition stx)
-  (syntax-parse stx
-    [(_ a) (dict-ref prim-table #'a #'(current-non-prim-transform a))]))
-
-(define-syntax backprop-out
-  (make-provide-pre-transformer
-   (λ (stx modes)
-     (syntax-parse stx
-       [(_ p ...)
-        (begin
-          (map syntax-local-lift-module-end-declaration
-               (syntax-e #'((register-primitive! p) ...)))
-          (pre-expand-export #'(combine-out p ...) modes))]))))
-
-(define-syntax-rule (require/primal+backprop path
-                                             [prim-id prim-augmented-def] ...)
-  (begin
-    (require (only-in path prim-id ...))
-    (local-register-primitive! prim-id prim-augmented-def) ...))
 
 (begin-for-syntax
   (define-syntax-class prim-spec
@@ -72,6 +31,52 @@
     (pattern (prim-id:id xs:id ...+ . xs*:id)
              #:attr vars #'(xs ... . xs*)
              #:attr appl #'(apply prim-id xs ... xs*))))
+
+;; ----------------------------------------
+
+(define-for-syntax prim-table (make-free-id-table))
+
+(define-for-syntax (get-prim-definition id)
+  (dict-ref prim-table id #f))
+
+(define-for-syntax (set-prim-definition! prim-id prim-augmented-def)
+  (dict-set! prim-table prim-id prim-augmented-def))
+
+;; ----------------------------------------
+
+(define-syntax (local-register-primitive! stx)
+  (syntax-parse stx
+    [(_ prim-id prim-augmented-def)
+     (set-prim-definition! #'prim-id #'prim-augmented-def)
+     #'(void)]))
+
+(define-syntax (register-primitive! stx)
+  (syntax-parse stx
+    [(_ prim-id)
+     #:with prim-augmented-def (get-prim-definition #'prim-id)
+     #'(begin-for-syntax
+         (set-prim-definition! #'prim-id #'prim-augmented-def))]))
+
+;; ----------------------------------------
+
+(define-syntax backprop-out
+  (make-provide-pre-transformer
+   (λ (stx modes)
+     (syntax-parse stx
+       [(_ p ...)
+        (begin
+          (map syntax-local-lift-module-end-declaration
+               (syntax-e #'((register-primitive! p) ...)))
+          (pre-expand-export #'(combine-out p ...) modes))]))))
+
+;; ----------------------------------------
+
+(define-syntax-parse-rule (require/primal+backprop
+                           path
+                           [prim-id prim-augmented-def] ...)
+  (begin
+    (require (only-in path prim-id ...))
+    (local-register-primitive! prim-id prim-augmented-def) ...))
 
 (define-syntax-parse-rule (require/backprop
                            path
